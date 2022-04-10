1de6f9408cc1b988a1c8d72cac0ef7642643fa1e
tdesero <tom.deser@googlemail.com>
2022-04-10 09:52:26 +0200

    unset conflicting css inside variants

--- src/code.ts
+++ src/code.ts
@@ -216,13 +216,24 @@ function getTreeElementByName(tree, name) {
 }
 
 function eraseDuplicateCSS(modifierCSS: string, baseCSS: string) {
-  const modArr = modifierCSS.split(";");
-  const baseArr = baseCSS.split(";");
+  const modArr = modifierCSS.split(";").map((l) => l.trim());
+  const baseArr = baseCSS.split(";").map((l) => l.trim());
+
+  // detect css lines included in base but not in modifier and unset the value
+  const cssPropsToBeUnset = baseArr
+    .map((l) => {
+      return l.split(":")?.[0];
+    })
+    .filter((prop) => {
+      return !modifierCSS.includes(`${prop}:`);
+    })
+    .map((prop) => prop + ": unset");
 
   return modArr
     .filter((line) => {
       return !baseArr.includes(line);
     })
+    .concat(cssPropsToBeUnset)
     .map((l) => l + ";")
     .join("");
 }
